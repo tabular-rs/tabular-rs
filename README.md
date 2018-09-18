371357caf800e23c0a48c88f8ebc855a9e9f7b56
Jesse A. Tov <jesse.tov@gmail.com>
2018-09-18 11:04:38 -0700

    README.md and lib.rs have little example.

--- README.md
+++ README.md
@@ -1,5 +1,53 @@
 # tabular: plain text tables in Rust
 
+Builds plain, automatically-aligned tables of monospaced text.
+This is basically what you want if you are implementing `ls`.
+
+## Example
+
+```rust
+use tabular::{Table, Row};
+
+fn ls(dir: &::std::path::Path) -> ::std::io::Result<()> {
+    let mut table = Table::new("{:>}  {:<}{:<}  {:<}");
+    for entry_result in ::std::fs::read_dir(dir)? {
+        let entry    = entry_result?;
+        let metadata = entry.metadata()?;
+
+        table.add_row(Row::new()
+             .with_cell(metadata.len())
+             .with_cell(if metadata.permissions().readonly() {"r"} else {""})
+             .with_cell(if metadata.is_dir() {"d"} else {""})
+             .with_cell(entry.path().display()));
+    }
+
+    print!("{}", table);
+
+    Ok(())
+}
+
+ls(Path::new(&"target")).unwrap();
+```
+
+produces something like
+
+```
+ 1198     target/.rustc_info.json
+ 1120  d  target/doc
+  672  d  target/debug
+```
+
+## Usage
+
+It's on [crates.io](https://crates.io/crates/tabular), so you can add
+
+```toml
+[dependencies]
+tabular = "0.1.0"
+```
+
+to your `Cargo.toml`.
+
 ## See also
 
 You may also want:
@@ -10,7 +58,4 @@ You may also want:
   
   - https://crates.io/crates/prettytable-rs — This has an API more
     similar to tabular’s in terms of building a table, but it does
-    a lot more, including color and borders. 
-  
-Both of these packages 
-  
+    a lot more, including, color, borders, and CSV import.
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,40 @@
 #![doc(html_root_url = "https://docs.rs/tabular/0.1.0")]
 //! Builds plain, automatically-aligned tables of monospaced text.
-//!
 //! This is basically what you want if you are implementing `ls`.
 //!
-//! # Details
+//! # Example
 //!
-//! The number and alignment of the columns is determined by a format string
-//! passed to [`Table::new`]. Then, [`Row`]s are added to the [`Table`]
-//! using [`Table::add_row`] or [`Table::with_row`]. Each row is is constructed
-//! by [`Row::new`] or `[Row::from_cells]`
+//! ```
+//! use tabular::{Table, Row};
 //!
-//! # Examples
+//! fn ls(dir: &::std::path::Path) -> ::std::io::Result<()> {
+//!     let mut table = Table::new("{:>}  {:<}{:<}  {:<}");
+//!     for entry_result in ::std::fs::read_dir(dir)? {
+//!         let entry    = entry_result?;
+//!         let metadata = entry.metadata()?;
+//!
+//!         table.add_row(Row::new()
+//!              .with_cell(metadata.len())
+//!              .with_cell(if metadata.permissions().readonly() {"r"} else {""})
+//!              .with_cell(if metadata.is_dir() {"d"} else {""})
+//!              .with_cell(entry.path().display()));
+//!     }
+//!
+//!     print!("{}", table);
+//!
+//!     Ok(())
+//! }
+//!
+//! ls(Path::new(&"target")).unwrap();
+//! ```
+//!
+//! produces something like
+//!
+//! ```text
+//!  1198     target/.rustc_info.json
+//!  1120  d  target/doc
+//!   672  d  target/debug
+//! ```
 //!
 //! # Usage
 //! 
@@ -22,6 +46,18 @@
 //! ```
 //!
 //! to your `Cargo.toml`.
+//!
+//! # See also
+//!
+//! You may also want:
+//!
+//! - [text-tables](https://crates.io/crates/text-tables) – This is more automatic
+//! than tabular. You give it an array of arrays, it renders a nice table with borders.
+//! Tabular doesn't do borders.
+//!
+//! - [prettytable](https://crates.io/crates/prettytable-rs) — This has an API more
+//! similar to tabular’s in terms of building a table, but it does a lot more, including,
+//! color, borders, and CSV import.
 //!
 //! [`Row`]: struct.Row.html
 //! [`Table`]: struct.Table.html
@@ -88,7 +124,5 @@ ab bc cd
     }
 
     #[test]
-    fn temporary() {
-      
-    }
+    fn temporary() {}
 }
