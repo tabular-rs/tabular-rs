bbb49d35036ed9048c651d62b3744634d209b6a0
Daniel Schmidt <danielmschmidt92@gmail.com>
2021-03-16 22:25:53 +0100

    fix: colored text length was off

--- src/width_string.rs
+++ src/width_string.rs
@@ -9,10 +9,13 @@ pub struct WidthString {
 impl WidthString {
     pub fn new<T: ToString>(thing: T) -> Self {
         let string = thing.to_string();
+        let string_without_color = strip_ansi_escapes::strip(&string)
+            .map(|str| String::from_utf8(str).unwrap_or_else(|_| thing.to_string()))
+            .unwrap_or_else(|_| thing.to_string());
         #[cfg(feature = "unicode-width")]
-        let width  = ::unicode_width::UnicodeWidthStr::width(string.as_str());
+        let width = ::unicode_width::UnicodeWidthStr::width(string_without_color.as_str());
         #[cfg(not(feature = "unicode-width"))]
-        let width  = string.chars().count();
+        let width = string_without_color.chars().count();
         WidthString { string, width }
     }
 
@@ -37,5 +40,29 @@ impl Default for WidthString {
             string: String::new(),
             width: 0,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use colored::*;
+
+    #[test]
+    fn simple_string() {
+        assert_eq!(WidthString::new("hello world").width, 11)
+    }
+
+    #[test]
+    fn unicode_string() {
+        assert_eq!(WidthString::new("unicode 🚀").width, 10)
+    }
+
+    #[test]
+    fn colored_string() {
+        assert_eq!(
+            WidthString::new("hello world").width,
+            WidthString::new("hello world".yellow()).width
+        )
     }
 }
