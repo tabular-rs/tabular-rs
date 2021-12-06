f693721c034481e5c291e55acf8dfe6116f74c20
Rain <rain@sunshowers.io>
2021-12-05 16:57:06 -0800

    add methods for ANSI and custom width cells
    Add new methods for cells with ANSI escapes, and cells with custom
    widths.

--- src/width_string.rs
+++ src/width_string.rs
@@ -9,11 +9,25 @@ pub struct WidthString {
 impl WidthString {
     pub fn new<T: ToString>(thing: T) -> Self {
         let string = thing.to_string();
-        #[cfg(feature = "unicode-width")]
-        let width  = ::unicode_width::UnicodeWidthStr::width(string.as_str());
-        #[cfg(not(feature = "unicode-width"))]
-        let width  = string.chars().count();
+        let width = Self::compute_width(&string);
         WidthString { string, width }
+    }
+
+    pub fn new_ansi<T: ToString>(thing: T) -> Self {
+        let string = thing.to_string();
+        let stripped_bytes =
+            strip_ansi_escapes::strip(&string).expect("writing to a Cursor<Vec<u8>> is infallible");
+        let stripped_string = String::from_utf8(stripped_bytes)
+            .expect("a UTF-8 string with ANSI escapes stripped is valid UTF-8");
+        let width = Self::compute_width(&stripped_string);
+        WidthString { string, width }
+    }
+
+    pub fn custom_width<T: ToString>(thing: T, width: usize) -> Self {
+        WidthString {
+            string: thing.to_string(),
+            width,
+        }
     }
 
     pub fn width(&self) -> usize {
@@ -22,6 +36,16 @@ impl WidthString {
 
     pub fn as_str(&self) -> &str {
         &self.string
+    }
+
+    #[cfg(feature = "unicode-width")]
+    fn compute_width(s: &str) -> usize {
+        unicode_width::UnicodeWidthStr::width(s)
+    }
+
+    #[cfg(not(feature = "unicode-width"))]
+    fn compute_width(s: &str) -> usize {
+        s.chars().count()
     }
 }
 
@@ -37,5 +61,21 @@ impl Default for WidthString {
             string: String::new(),
             width: 0,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::width_string::WidthString;
+    use colored::Colorize;
+
+    #[test]
+    fn ansi() {
+        let s = "blue";
+        assert_eq!(WidthString::new_ansi(s).width, 4);
+        let colored = s.blue();
+        assert_eq!(WidthString::new_ansi(&colored).width, 4);
+        let bolded = colored.bold();
+        assert_eq!(WidthString::new_ansi(&bolded).width, 4);
     }
 }
